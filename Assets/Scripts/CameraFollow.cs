07b8e461aeda0d4be329dab5a6e9d0404fedd6eb
joshuawisdom <78412743+joshuawisdom@users.noreply.github.com>
2021-04-25 13:08:38 -0700

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+    }
+
+
+}
